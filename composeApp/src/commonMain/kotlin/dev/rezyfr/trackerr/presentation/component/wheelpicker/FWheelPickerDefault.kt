40cb059089f4c35aea5189843b65aaba37050f76
rezyfr <frizkillah98@gmail.com>
2023-09-09 13:46:12 +0700

    Add wheelpicker

---
+++ composeApp/src/commonMain/kotlin/dev/rezyfr/trackerr/presentation/component/wheelpicker/FWheelPickerDefault.kt
@@ -0,0 +1,73 @@
+package dev.rezyfr.trackerr.presentation.component.wheelpicker
+
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+/**
+ * The default implementation of focus view in vertical.
+ */
+@Composable
+fun FWheelPickerFocusVertical(
+    modifier: Modifier = Modifier,
+    dividerSize: Dp = 1.dp,
+    dividerColor: Color = DefaultDividerColor,
+) {
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .background(dividerColor)
+                .height(dividerSize)
+                .fillMaxWidth()
+                .align(Alignment.TopCenter),
+        )
+        Box(
+            modifier = Modifier
+                .background(dividerColor)
+                .height(dividerSize)
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter),
+        )
+    }
+}
+
+private val DefaultDividerColor: Color
+    @Composable
+    get() = (if (isSystemInDarkTheme()) {
+        Color.White
+    } else {
+        Color.Black
+    }).copy(alpha = 0.2f)
+
+/**
+ * Default content wrapper.
+ */
+val DefaultWheelPickerContentWrapper: @Composable FWheelPickerContentWrapperScope.(index: Int) -> Unit
+    get() = { index ->
+        val isFocus = index == state.currentIndexSnapshot
+        val targetAlpha = if (isFocus) 1.0f else 0.3f
+        val targetScale = if (isFocus) 1.0f else 0.8f
+        val animateScale by animateFloatAsState(targetScale)
+        Box(
+            modifier = Modifier
+                .graphicsLayer {
+                    this.alpha = targetAlpha
+                    this.scaleX = animateScale
+                    this.scaleY = animateScale
+                }
+        ) {
+            content(index)
+        }
+    }
