defa5b22d8dcc4d7abd71b0adbdf252e0923006d
rezyfr <frizkillah98@gmail.com>
2023-09-10 13:57:48 +0700

    Clean up commented code

--- composeApp/src/iosMain/kotlin/dev/rezyfr/trackerr/presentation.component.util/IosStringExt.kt
+++ composeApp/src/iosMain/kotlin/dev/rezyfr/trackerr/presentation.component.util/IosStringExt.kt
@@ -13,10 +13,3 @@ actual fun String.toDateFormat(
     dateFormatter.dateFormat = newFormat
     return dateFormatter.stringFromDate(currentDate!!)
 }
-//let formatter = Foundation.DateFormatter()
-//formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'" //2017-04-01T18:05:00.000
-//let date1  = formatter.date(from: "2017-04-01T18:05:00.000Z")
-//print("date:\(String(describing: date1))")
-//formatter.dateFormat = "HH:mm"
-//let resultTime = formatter.string(from: date1!)
-//print("time:\(String(describing: resultTime))")
